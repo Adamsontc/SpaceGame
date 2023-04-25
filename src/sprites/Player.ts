15c83d890573defc77d332d360d5713a88d15d24
Henry Roeth <roethhk@hiram.edu>
2023-04-24 21:00:14 -0400

    Message.

--- src/sprites/Player.ts
+++ src/sprites/Player.ts
@@ -10,8 +10,8 @@ export class Player extends Creature {
 
     constructor() {
         super();
-        this.MAX_SPEED=1.0;
-        this.JUMP_SPEED=0.95 ;
+        this.MAX_SPEED=.5;
+        this.JUMP_SPEED=0.75 ;
         this.onGround=false;
     }
 
