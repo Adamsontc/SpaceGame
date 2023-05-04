d040648e0cbca858ec6120c829e4e83e3188a356
Louis Oliphant <oliphantlt@hiram.edu>
2023-05-04 10:30:21 -0400

    added blast.png and Projectile.ts files

---
+++ src/sprites/Projectile.ts
@@ -0,0 +1,23 @@
+import { Sprite } from './Sprite.js';
+
+
+export class Projectile extends Sprite {
+
+    constructor() {
+        super();
+        this.setVelocity(0.25,0);
+    }
+
+    isFlying() {
+        return true;
+    }
+
+    setRight(isRight:boolean) {
+        if (isRight) {
+            this.velocity.x=Math.abs(this.velocity.x);
+        } else {
+            this.velocity.x=-Math.abs(this.velocity.x);
+        }
+    }
+
+}
